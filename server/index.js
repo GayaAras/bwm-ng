3206b3bc2c5489fbba866a879fecb030149ce075
Gayatri Arasaratnam <GayatriArasaratnam@Gayatris-MacBook-Pro.local>
2019-02-19 12:46:02 -0800

    Adds booking creation functionality, adds improvements of rental routes

--- server/index.js
+++ server/index.js
@@ -6,7 +6,8 @@ const FakeDb = require ('./fake-db');
 const Rental = require ('./models/rental');
 
 const rentalRoutes = require('./routes/rentals'),
-      userRoutes = require('./routes/users');
+      userRoutes = require('./routes/users'),
+      bookingRoutes = require('./routes/bookings');
 
 //mongoose.connect('mongodb://Test:testtest@bwm-ng-dev-shard-00-00-ihzah.mongodb.net:27017,bwm-ng-dev-shard-00-01-ihzah.mongodb.net:27017,bwm-ng-dev-shard-00-02-ihzah.mongodb.net:27017/test?ssl=true&replicaSet=bwm-ng-dev-shard-0&authSource=admin&retryWrites=true', { useNewUrlParser: true })
 
@@ -21,6 +22,7 @@ app.use(bodyParser.json());
 
 app.use('/api/v1/rentals', rentalRoutes);
 app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/bookings', bookingRoutes);
 
 const PORT = process.env.PORT || 3001;
 
