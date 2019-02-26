52fe96ae40318d94be2c3457319d5203c9865dfd
Gayatri Arasaratnam <GayatriArasaratnam@Gayatris-MBP.attlocal.net>
2019-02-26 14:53:27 -0800

    Adds pre-deploy setup

--- server/index.js
+++ server/index.js
@@ -1,19 +1,20 @@
 const express = require('express');
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const config = require ('./config/dev');
-const FakeDb = require ('./fake-db');
-const Rental = require ('./models/rental');
+const config = require('./config');
+const FakeDb = require('./fake-db');
+const Rental = require('./models/rental');
+const path = require('path');
 
 const rentalRoutes = require('./routes/rentals'),
       userRoutes = require('./routes/users'),
       bookingRoutes = require('./routes/bookings');
 
-//mongoose.connect('mongodb://Test:testtest@bwm-ng-dev-shard-00-00-ihzah.mongodb.net:27017,bwm-ng-dev-shard-00-01-ihzah.mongodb.net:27017,bwm-ng-dev-shard-00-02-ihzah.mongodb.net:27017/test?ssl=true&replicaSet=bwm-ng-dev-shard-0&authSource=admin&retryWrites=true', { useNewUrlParser: true })
-
-mongoose.connect(config.DB_URI, { useNewUrlParser: true }).then(() => {
+mongoose.connect(config.DB_URI).then(() => {
+   if (process.env.NODE_ENV !== 'production') {
     const fakeDb = new FakeDb();
     //fakeDb.seedDb();
+   }
 });
 
 const app = express();
@@ -24,8 +25,17 @@ app.use('/api/v1/rentals', rentalRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/bookings', bookingRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  const appPath = path.join(__dirname, '..', 'dist');
+  app.use(express.static(appPath));
+
+  app.get('*', function(req, res) {
+    res.sendFile(path.resolve(appPath, 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, function(){
-    console.log('I am running');
+app.listen(PORT , function() {
+  console.log('App is running!');
 });
